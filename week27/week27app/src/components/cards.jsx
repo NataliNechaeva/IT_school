d797c047b520a977fc87bcb0c1ef5cb8919a15c4
NataliNechaeva <barylia@icloud.com>
2022-01-06 18:47:59 +0500

    WEEK28

--- week27/week27app/src/components/cards.jsx
+++ week27/week27app/src/components/cards.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import './cards.scss';
+import { Button } from 'antd';
 
 
 class Rate extends React.Component {
+  state = {
+    checked: false,
+  };
+  toggleChecked = () => {
+    this.setState({ checked: !this.state.checked });
+  };
   render () {
-    const {title,price,value,description,isSelected} = this.props;
+    const {title,price,value,description} = this.props;
     let rateClass="";
-    if(isSelected) rateClass="selected";
+    if(this.state.checked) rateClass="selected";
     return (
-    <div className={`rate ${rateClass}`}>
+  <div className={`rate ${rateClass}`}>
   <h1 className="rate-title">{title}</h1>
   <h2 className="rate-price">{price}</h2>
   <h3 className="rate-value">{value}</h3>
   <p className="rate-description">{description}</p>
+  <Button type="primary" size="large"onClick={this.toggleChecked}> {!this.state.checked ? 'Не выбрано' : 'Мой тариф'}</Button>
+          
   </div>    
   )
 }
